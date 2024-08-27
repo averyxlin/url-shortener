b61a9b5df54bdaab78d6a00229d6bea1589d64b5
Personal <xinyan.lin@uwaterloo.ca>
2024-08-27 00:35:20 -0400

    logo

---
+++ src/components/atoms/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span className="logo-url">URL</span>
+      <span className="logo-shortener">SHORTENER</span>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/organisms/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+import Logo from '../../atoms/Logo/Logo';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-section">
+        <Logo />
+      </div>
+      <div className="header-section"></div>
+    </div>
+  );
+};
+
+export default Header;
